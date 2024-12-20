53808858f105ea41a642c285fa9f9d606e7fd6df
Selim Reja <48088144+SelimRejabd@users.noreply.github.com>
2024-12-20 15:39:39 +0600

    Admin

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,10 +1,10 @@
-import { UserService } from "./user.service";
+import { UserServices } from "./user.service";
 import sendResponse from "../../utils/sendResponse";
 import catchAsync from "../../utils/catchAsync";
 
 const createStudent = catchAsync(async (req, res) => {
   const { password, studentData } = req.body;
-  const result = await UserService.createStudentIntoDB(password, studentData);
+  const result = await UserServices.createStudentIntoDB(password, studentData);
   sendResponse(res, {
     statusCode: 200,
     success: true,
@@ -13,6 +13,34 @@ const createStudent = catchAsync(async (req, res) => {
   });
 });
 
-export const UserController = {
+const createFaculty = catchAsync(async (req, res) => {
+  const { password, faculty: facultyData } = req.body;
+
+  const result = await UserServices.createFacultyIntoDB(password, facultyData);
+
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Faculty is created succesfully",
+    data: result,
+  });
+});
+
+const createAdmin = catchAsync(async (req, res) => {
+  const { password, admin: adminData } = req.body;
+
+  const result = await UserServices.createAdminIntoDB(password, adminData);
+
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Admin is created succesfully",
+    data: result,
+  });
+});
+
+export const UserControllers = {
   createStudent,
+  createFaculty,
+  createAdmin,
 };
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,14 +1,28 @@
 import express from "express";
-import { UserController } from "./user.controller";
+import { UserControllers } from "./user.controller";
 import { StudentValidations } from "../student/student.validation";
 import validateRequest from "../../middleware/validateRequest";
+import { createFacultyValidationSchema } from "../Faculty/faculty.validation";
+import { createAdminValidationSchema } from "../Admin/admin.validation";
 
 const router = express.Router();
 
 router.post(
   "/create-student",
   validateRequest(StudentValidations.CreateStudentValidationSchema),
-  UserController.createStudent
+  UserControllers.createStudent
+);
+
+router.post(
+  "/create-faculty",
+  validateRequest(createFacultyValidationSchema),
+  UserControllers.createFaculty
+);
+
+router.post(
+  "/create-admin",
+  validateRequest(createAdminValidationSchema),
+  UserControllers.createAdmin
 );
 
 export const UserRoutes = router;
